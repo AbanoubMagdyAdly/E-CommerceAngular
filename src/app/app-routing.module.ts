64609edb2dcb05dc7d77e290147c116e69b05885
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-04-04 13:39:04 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './components/app/app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { WishListComponent } from './components/wish-list/wish-list.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductComponent } from './components/product/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: ''               , component : HomeComponent},
+  {path: 'login'          , component : LoginComponent},
+  {path: 'cart'           , component : CartComponent},
+  {path: 'signup'         , component : SignupComponent},
+  {path: 'wishlist'       , component : WishListComponent},
+  {path: 'product'        , component : ProductComponent},
+  {path: '**'             , component : NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartComponent } from './components/cart/cart.component';
+import { ProductComponent } from './components/product/product.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { WishListComponent } from './components/wish-list/wish-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    HomeComponent,
+    CartComponent,
+    ProductComponent,
+    LoginComponent,
+    SignupComponent,
+    WishListComponent
   ],
   imports: [
     BrowserModule,
