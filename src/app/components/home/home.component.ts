5457ad7d1f6564126a87ccde67c417e1f7d6e5bf
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-04-08 10:24:51 +0200

    add cart & wishlist count

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { StreamService } from 'src/app/stream.service';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
   products: object[];
-  constructor(private http: HttpClient,private router : Router) { 
+  constructor(private http: HttpClient,private router : Router,private stream: StreamService) { 
     let name = localStorage.getItem('name');
     if(name===null||name===undefined){
       this.router.navigate(['']);
@@ -20,6 +21,19 @@ export class HomeComponent implements OnInit {
         .subscribe(data => {
         this.products = data["arrayOfProducts"];
       });
+  }
+  addToCart(product){
+    let products = JSON.parse(localStorage.getItem('cart'))  || [];
+    products.push(product);
+    localStorage.setItem('cart',JSON.stringify(products))
+    this.stream.setcartCount(products.length)
+  }
+  addToWishList(product){
+    let products = JSON.parse(localStorage.getItem('wishlist')) || [];
+    products.push(product);
+    localStorage.setItem('wishlist',JSON.stringify(products))
+    this.stream.setwishListCount(products.length)
+
   }
 
 }
