f39a1ef25ba511e49c77a2b6478851fbcada9abb
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-04-09 18:34:22 +0200

    Modify cart Components

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { StreamService } from 'src/app/stream.service';
 
 @Component({
   selector: 'app-cart',
@@ -8,12 +9,25 @@ import { Router } from '@angular/router';
   styleUrls: ['./cart.component.sass']
 })
 export class CartComponent implements OnInit {
-
-  constructor(private http: HttpClient,private router : Router) { 
+  public cartProducts;
+  constructor(private http: HttpClient, private router: Router, private stream: StreamService) {
     let name = localStorage.getItem('name');
-    if(name===null||name===undefined){
+    if (name === null || name === undefined) {
       this.router.navigate(['']);
     }
+    let products;
+    this.stream.getProducts().subscribe(res => products = res);
+    function mapToProp(data, prop) {
+      return data
+        .reduce((res, item) => Object
+          .assign(res, {
+            [item[prop]]: 1 + (res[item[prop]] || 0)
+          }), Object.create(null))
+        ;
+    }
+    let productCount = mapToProp(products, 'id')
+    console.log(productCount);
+    this.stream.getProducts().subscribe(res => this.cartProducts = res)
   }
   ngOnInit() {
   }
