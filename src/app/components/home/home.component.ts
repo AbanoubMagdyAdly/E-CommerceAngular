f52c8cb232f92952aa62876c697d732e9cbc2178
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-04-09 18:34:40 +0200

    Modify home Components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,30 +10,30 @@ import { StreamService } from 'src/app/stream.service';
 })
 export class HomeComponent implements OnInit {
   products: object[];
-  constructor(private http: HttpClient,private router : Router,private stream: StreamService) { 
+  constructor(private http: HttpClient, private router: Router, private stream: StreamService) {
     let name = localStorage.getItem('name');
-    if(name===null||name===undefined){
+    if (name === null || name === undefined) {
       this.router.navigate(['']);
     }
   }
   ngOnInit() {
     this.http.get('../../../assets/products.json')
-        .subscribe(data => {
+      .subscribe(data => {
         this.products = data["arrayOfProducts"];
       });
   }
-  addToCart(product){
-    let products = JSON.parse(localStorage.getItem('cart'))  || [];
+  addToCart(product) {
+    let products = JSON.parse(localStorage.getItem('cart')) || [];
     products.push(product);
-    localStorage.setItem('cart',JSON.stringify(products))
+    localStorage.setItem('cart', JSON.stringify(products))
     this.stream.setcartCount(products.length)
+    this.stream.setProducts(JSON.stringify(products))
   }
-  addToWishList(product){
+  addToWishList(product) {
     let products = JSON.parse(localStorage.getItem('wishlist')) || [];
     products.push(product);
-    localStorage.setItem('wishlist',JSON.stringify(products))
+    localStorage.setItem('wishlist', JSON.stringify(products))
     this.stream.setwishListCount(products.length)
-
   }
 
 }
