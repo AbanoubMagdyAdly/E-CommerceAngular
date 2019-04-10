eb4b160302f2152641d554805407e31177752ffb
AbanoubMagdyAdly <abanoub.magdy.adly@gmail.com>
2019-04-10 15:15:50 +0200

    modfiy stream service

--- src/app/stream.service.ts
+++ src/app/stream.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Item } from './item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StreamService {
   private name: BehaviorSubject<string> = new BehaviorSubject('');
-  private cartCount: BehaviorSubject<string> = new BehaviorSubject('');
+  private cartCount: BehaviorSubject<number> = new BehaviorSubject(0);
   private wishListCount: BehaviorSubject<string> = new BehaviorSubject('');
-  private products: BehaviorSubject<string> = new BehaviorSubject('');
+  private products: BehaviorSubject<Item[]> = new BehaviorSubject([]);
 
   constructor() {
     this.name.next(localStorage.getItem('name'))
     let cartproducts = JSON.parse(localStorage.getItem('cart')) || [];
     let wishListproducts = JSON.parse(localStorage.getItem('wishlist')) || [];
-    this.cartCount.next(cartproducts.length)
+    this.cartCount.next(this.calculateTotalItems())
     this.wishListCount.next(wishListproducts.length)
     this.products.next(cartproducts)
 
@@ -22,14 +23,14 @@ export class StreamService {
   setName(val?: string): void {
     this.name.next(val);
   }
-  setcartCount(val?: string): void {
+  setcartCount(val?: number): void {
     this.cartCount.next(val);
   }
   setwishListCount(val?: string): void {
     this.wishListCount.next(val);
   }
 
-  setProducts(val?: string): void {
+  setProducts(val?: Item[]): void {
     this.products.next(val);
   }
 
@@ -44,5 +45,15 @@ export class StreamService {
   }
   getProducts() {
     return this.products.asObservable();
+  }
+  calculateTotalItems(): number {
+    let totalQty: number = 0;
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    if (cart) {
+      cart.map(function(item: Item) {
+        totalQty += item.quantity;
+      });
+    }
+    return totalQty;
   }
 }
